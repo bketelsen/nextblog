e30017fc61e5a05a4d6434fd3b25bcca9e2d48d4
Brian Ketelsen <bketelsen@gmail.com>
2021-07-13 06:44:50 -0400

    fix edit link

--- layouts/AppearanceLayout.js
+++ layouts/AppearanceLayout.js
@@ -6,7 +6,7 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 
-const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
+const editUrl = (fileName) => `${siteMetadata.siteRepo}/elsewhere/master/data/elsewhere/${fileName}`
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
     `${siteMetadata.siteUrl}/elsewhere/${slug}`
