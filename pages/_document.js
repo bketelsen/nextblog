52937b90cbbc359ae51c84b4ef112b07a9da9843
Brian Ketelsen <bketelsen@gmail.com>
2021-08-14 07:52:06 -0400

    plausible

--- pages/_document.js
+++ pages/_document.js
@@ -22,17 +22,33 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+
           <link
+            rel="preload"
+            as="style"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            rel="stylesheet"
           />
+
           <link
             rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
-            integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
-            crossOrigin="anonymous"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            media="print"
+            onLoad="this.media='all'"
           />
+          <script
+            async
+            defer
+            data-domain="brian.dev"
+            src="https://p.brian.dev/js/plausible.js"
+          ></script>
+
+          <noscript>
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            />
+          </noscript>
         </Head>
         <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
           <Main />
