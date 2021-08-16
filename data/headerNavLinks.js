fa066de21e8a13c74e7fff1029632aab14bf2325
Brian Ketelsen <bketelsen@gmail.com>
2021-08-16 05:42:45 -0400

    remove projects

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/elsewhere', title: 'Elsewhere' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
