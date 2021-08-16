c828d6547840b101278810ee50bfda2c694cfb37
Brian Ketelsen <bketelsen@gmail.com>
2021-08-16 08:02:53 -0400

    add google font npm package

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,13 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { GoogleFonts } from 'nextjs-google-fonts/GoogleFonts'
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          {GoogleFonts()}
+
           <meta name="theme-color" content="red" />
           <meta name="mobile-web-app-capable" content="yes" />
 
@@ -43,29 +47,6 @@ class MyDocument extends Document {
 
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#000000" />
-          <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link
-            rel="preload"
-            as="style"
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-          />
-          <link
-            media="print"
-            onLoad="this.media='all'"
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-            rel="stylesheet"
-          />
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-            />
-          </noscript>
         </Head>
         <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
           <Main />
