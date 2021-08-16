3392385dca4a30d6a2542204cc060f70a099a715
Brian Ketelsen <bketelsen@gmail.com>
2021-08-16 17:00:01 +0000

    banner

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/bketelsen/nextblog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/briandev.png',
   email: 'mail@bjk.fyi',
   github: 'https://github.com/bketelsen',
   twitter: 'https://twitter.com/bketelsen',
