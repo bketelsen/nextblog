59897b7d76f74fdab69fcfcd219a6df3e0597b17
Brian Ketelsen <bketelsen@gmail.com>
2021-08-21 10:36:40 +0000

    add moar icons

--- pages/_document.js
+++ pages/_document.js
@@ -44,13 +44,36 @@ class MyDocument extends Document {
 
           <meta name="screen-orientation" content="portrait" />
 
-          <link href="/icons/icon-72x72.png" rel="icon" type="image/png" sizes="72x72" />
-          <link href="/icons/icon-96x96.png" rel="icon" type="image/png" sizes="96x96" />
-          <link href="/icons/icon-128x128.png" rel="icon" type="image/png" sizes="128x128" />
-          <link href="/icons/icon-144x144.png" rel="icon" type="image/png" sizes="144x144" />
-          <link href="/icons/icon-192x192.png" rel="icon" type="image/png" sizes="192x192" />
-
-          <link href="/icons/icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+          <link
+            href="/icons/android-launchericon-72-72.png"
+            rel="icon"
+            type="image/png"
+            sizes="72x72"
+          />
+          <link
+            href="/icons/android-launchericon-96-96.png"
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+          />
+          <link
+            href="/icons/firefox-general-128-128.png"
+            rel="icon"
+            type="image/png"
+            sizes="128x128"
+          />
+          <link
+            href="/icons/android-launchericon-144-144.png"
+            rel="icon"
+            type="image/png"
+            sizes="144x144"
+          />
+          <link
+            href="/icons/android-launchericon-192-192.png"
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+          />
 
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#000000" />
