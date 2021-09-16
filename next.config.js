c2e304883e33faf5bd701785d9fbf9d166f6b430
Brian Ketelsen <bketelsen@gmail.com>
2021-09-16 17:26:06 -0400

    more removing

--- next.config.js
+++ next.config.js
@@ -2,60 +2,57 @@ const withPWA = require('next-pwa')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-const { withPlausibleProxy } = require('next-plausible')
 const { withGoogleFonts } = require('nextjs-google-fonts')
 
 module.exports = withGoogleFonts(
   withPWA(
-    withPlausibleProxy()(
-      withBundleAnalyzer({
-        googleFonts: {
-          fonts: [
-            'https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;600;700&display=optional',
-          ],
-        },
-        images: {
-          domains: ['res.cloudinary.com'],
-        },
-        pwa: {
-          dest: 'public',
-          disable: process.env.NODE_ENV === 'development',
-        },
-        reactStrictMode: true,
-        pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-        eslint: {
-          dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
-        },
-        experimental: { esmExternals: true },
-        webpack: (config, { dev, isServer }) => {
-          config.module.rules.push({
-            test: /\.(png|jpe?g|gif|mp4)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  publicPath: '/_next',
-                  name: 'static/media/[name].[hash].[ext]',
-                },
+    withBundleAnalyzer({
+      googleFonts: {
+        fonts: [
+          'https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;600;700&display=optional',
+        ],
+      },
+      images: {
+        domains: ['res.cloudinary.com'],
+      },
+      pwa: {
+        dest: 'public',
+        disable: process.env.NODE_ENV === 'development',
+      },
+      reactStrictMode: true,
+      pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+      eslint: {
+        dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
+      },
+      experimental: { esmExternals: true },
+      webpack: (config, { dev, isServer }) => {
+        config.module.rules.push({
+          test: /\.(png|jpe?g|gif|mp4)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                publicPath: '/_next',
+                name: 'static/media/[name].[hash].[ext]',
               },
-            ],
-          })
+            },
+          ],
+        })
 
-          config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        })
+        if (!dev && !isServer) {
+          // Replace React with Preact only in client production build
+          Object.assign(config.resolve.alias, {
+            react: 'preact/compat',
+            'react-dom/test-utils': 'preact/test-utils',
+            'react-dom': 'preact/compat',
           })
-          if (!dev && !isServer) {
-            // Replace React with Preact only in client production build
-            Object.assign(config.resolve.alias, {
-              react: 'preact/compat',
-              'react-dom/test-utils': 'preact/test-utils',
-              'react-dom': 'preact/compat',
-            })
-          }
-          return config
-        },
-      })
-    )
+        }
+        return config
+      },
+    })
   )
 )
