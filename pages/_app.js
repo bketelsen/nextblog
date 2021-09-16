1a62bdad1aa5e3584a0d7a8e7063cfe4ea8e768b
Brian Ketelsen <bketelsen@gmail.com>
2021-09-16 17:19:59 -0400

    remove plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import '@/styles/globals.css'
 
 import Layout from '@/components/Layout'
-import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlausibleProvider domain="brian.dev">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </PlausibleProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
